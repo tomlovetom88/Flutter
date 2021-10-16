b30e9393ed71bb40cd8b3fe418550bc6ee54c638
Nopparat <41279326+tomlovetom88@users.noreply.github.com>
2021-10-16 23:14:04 +0700

    Add files via upload

---
+++ lib/screen/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:loginsystem/screen/login.dart';
+import 'package:loginsystem/screen/register.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nopparat"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                  icon: Icon(Icons.login),
+                  label: Text("Login", style: TextStyle(fontSize: 20)),
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) {
+                      return LoginScreen();
+                    }));
+                  },
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                  icon: Icon(Icons.add),
+                  label: Text("Register", style: TextStyle(fontSize: 20)),
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) {
+                      return RegisterScreen();
+                    }));
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
